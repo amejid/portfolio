899b10d8ca2fbaddc035139db9615eddd4de7a94
amejid <abdelmejid89@gmail.com>
2022-12-18 22:22:43 +0300

    Update project images

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,7 @@
-import propertyImg from '../public/assets/projects/property.jpg'
-import cryptoImg from '../public/assets/projects/crypto.jpg'
-import netflixImg from '../public/assets/projects/netflix.jpg'
-import twitchImg from '../public/assets/projects/twitch.jpg'
+import exoticImg from '../public/assets/projects/exotic.webp'
+import crownImg from '../public/assets/projects/crown.webp'
+import pokemonImg from '../public/assets/projects/pokemon.webp'
+import ytechImg from '../public/assets/projects/ytech.webp'
 
 import ProjectItem from "./ProjectItem";
 const Projects = () => {
@@ -11,10 +11,10 @@ const Projects = () => {
         <p className='text-xl tracking-widest uppercase text-[#5661e5]'>Projects</p>
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
-          <ProjectItem title='Property Finder' backgroundImg={propertyImg} projectUrl='/property' />
-          <ProjectItem title='Property Finder' backgroundImg={cryptoImg} projectUrl='/property' />
-          <ProjectItem title='Property Finder' backgroundImg={netflixImg} projectUrl='/property' />
-          <ProjectItem title='Property Finder' backgroundImg={twitchImg} projectUrl='/property' />
+          <ProjectItem title='Property Finder' backgroundImg={exoticImg} projectUrl='/property' />
+          <ProjectItem title='Property Finder' backgroundImg={crownImg} projectUrl='/property' />
+          <ProjectItem title='Property Finder' backgroundImg={pokemonImg} projectUrl='/property' />
+          <ProjectItem title='Property Finder' backgroundImg={ytechImg} projectUrl='/property' />
         </div>
       </div>
     </section>
