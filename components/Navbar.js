7acdc787d05fd3c838c3d77310ccacd292bfd74d
amejid <abdelmejid89@gmail.com>
2022-12-18 15:38:46 +0300

    Create navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,103 @@
+import Image from "next/image";
+import Link from "next/link";
+import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from "react-icons/ai";
+import {FaGithub, FaLinkedinIn} from "react-icons/fa";
+import {BsFillPersonLinesFill} from "react-icons/bs";
+import {useState} from "react";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav((prev) => !prev);
+  }
+
+  return (
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Image src="/../public/assets/navLogo.png" alt='' width='125' height='50'/>
+        <div>
+          <ul className='hidden md:flex'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/'>Home</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/'>About</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/'>Skills</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/'>Projects</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href='/'>Contact</Link>
+            </li>
+          </ul>
+          <div onClick={handleNav} className='cursor-pointer md:hidden'>
+            <AiOutlineMenu size={25}/>
+          </div>
+        </div>
+      </div>
+
+      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+        <div
+          className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
+          <div>
+            <div className='flex w-full items-center justify-between'>
+              <Image src='/../public/assets/navLogo.png' alt='/' width='87' height='35'/>
+              <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                <AiOutlineClose/>
+              </div>
+            </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w-[90%] py-4'>Let&apos;s build something together</p>
+            </div>
+          </div>
+          <div className='py-4 flex flex-col'>
+            <ul className='uppercase'>
+              <li className='py-4 text-sm'>
+                <Link href='/'>Home</Link>
+              </li>
+              <li className='py-4 text-sm'>
+                <Link href='/'>About</Link>
+              </li>
+              <li className='py-4 text-sm'>
+                <Link href='/'>Skills</Link>
+              </li>
+              <li className='py-4 text-sm'>
+                <Link href='/'>Projects</Link>
+              </li>
+              <li className='py-4 text-sm'>
+                <Link href='/'>Contact</Link>
+              </li>
+            </ul>
+            <div className='pt-40'>
+              <p className='uppercase tracking-widest text-[#5651e5]'>Let&apos;s Connect</p>
+              <ul className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                <li
+                  className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
+                  <FaLinkedinIn/>
+                </li>
+                <li
+                  className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
+                  <FaGithub/>
+                </li>
+                <li
+                  className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
+                  <AiOutlineMail/>
+                </li>
+                <li
+                  className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-500'>
+                  <BsFillPersonLinesFill/>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
