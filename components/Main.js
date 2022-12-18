484eb819dbac3b1bb35ea1f5cb2dcecdffa99ce0
amejid <abdelmejid89@gmail.com>
2022-12-18 15:57:20 +0300

    Create main section

---
+++ components/Main.js
@@ -0,0 +1,40 @@
+import {FaGithub, FaLinkedinIn} from "react-icons/fa";
+import {AiOutlineMail} from "react-icons/ai";
+import {BsFillPersonLinesFill} from "react-icons/bs";
+
+const Main = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+        <div>
+          <p className='uppercase text-sm tracking-widest text-gray-600'>LET&apos;S BUILD SOMETHING TOGETHER</p>
+          <h1 className='py-4 text-gray-700'>Hi, I&apos;m <span className='text-[#5651e5]'>Abdelmejid</span></h1>
+          <h2 className='py-2 text-gray-700'>A Full-Stack Web Developer</h2>
+          <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+
+          </p>
+          <ul className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <li
+              className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-500'>
+              <FaLinkedinIn/>
+            </li>
+            <li
+              className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-500'>
+              <FaGithub/>
+            </li>
+            <li
+              className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-500'>
+              <AiOutlineMail/>
+            </li>
+            <li
+              className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-500'>
+              <BsFillPersonLinesFill/>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
